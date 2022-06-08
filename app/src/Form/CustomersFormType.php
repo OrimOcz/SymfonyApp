eef6c26a25120d83d2982ebf4ec7a1222c0a80f0
Jaroslav Maňo <86402091+OrimOcz@users.noreply.github.com>
2022-06-08 17:09:33 +0200

    Install twig

---
+++ app/src/Form/CustomersFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customers;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomersFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phone', PhoneType::class)
+            ->add('Submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customers::class,
+        ]);
+    }
+}
